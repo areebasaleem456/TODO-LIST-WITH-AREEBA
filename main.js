0eab3fe60937da6a9cb4991a3645ff09ab72bf7f
AREEBASALEEM123 <166440229+AREEBASALEEM123@users.noreply.github.com>
2024-05-12 02:47:56 +0500

    Make TODO-APP with areeba

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todoList = [];
+let conditions = true;
+console.log("\n\t Welcome to Areeba TODO-APP\n");
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your task here:"
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} added in your list.`);
+    let moreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more tasks?",
+            default: "false"
+        }
+    ]);
+    conditions = moreTask.addmore;
+}
+;
+console.log(`Your updated todo List:${todoList}`);
